6ce08b32936035a3aa8f9b9c58f00fd4c798d7ca
Sheldon Demario <sheldon@telefonicabeta.com>
2015-04-10 10:58:55 -0300

    Adicionando exercicio 8

---
+++ Ex3_medias.py
@@ -0,0 +1,5 @@
+#Faca um programa que leia notas de diversos alunos e calcule a media
+#a lista termina quando e digitado um numero negativo
+
+# Exemplo de Saida esperada:
+# >>> Media: 6.7
---
+++ Ex8_medias.py
@@ -0,0 +1,7 @@
+#Copie o exercicio Ex3_medias.py que voce fez na aula passada e adicione uma
+#funcao que mostre a maior nota da lista. Eh necessario proteger o input do
+#usuario com tratamento de excecoes.
+
+#Exemplo de saida esperada:
+# >>> Media: 5.8
+# >>> Maior nota: 7.7
